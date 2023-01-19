f4e1c4727800e1ec778682441298a783324c9adc
David Webb <webbd5050@gmail.com>
2023-01-19 17:10:15 -0500

    finished styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css"
+import React, { Component } from "react"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Lottery from "./Lottery"
+
+export class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Lottery />
+        <Lottery title="Mini Daily" maxNum={10} numBalls={4} />
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
